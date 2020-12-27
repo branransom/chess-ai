b9f6b28688219afa40b476ca8eb90be83fc15821
Brandon Ransom <branransom@gmail.com>
2020-12-27 12:24:05 -0500

    Train CNN from chess PGN data

--- main.py
+++ main.py
@@ -1,54 +1,123 @@
 import chess
 import chess.pgn
-from evaluate import get_position_value, get_piece_value
+import numpy as np
+import tensorflow as tf
+import os
+import time
+from multiprocessing import Pool, TimeoutError
+from sklearn.model_selection import train_test_split
+from serialize import serialize
 
-def read_game():
-    pgn = open("data/Fischer.pgn")
 
-    first_game = chess.pgn.read_game(pgn)
-    print(first_game.headers["Event"])
+def read_game(file):
+    print(f'Reading games from {file}...')
+    pgn = open(file)
 
-    board = first_game.board()
-    for move in first_game.mainline_moves():
-        board.push(move)
-        print(board)
+    X = []
+    y = []
+    games_read = 0
 
-def play_game():
-    board = chess.Board()
+    result_states = { '1/2-1/2': 0, '0-1': -1, '1-0': 1 }
 
-    for i in range(50):
-        # if (board.turn == chess.WHITE):
-            # input_value = input("Press enter to continue, s to stop... ")
-            # if input_value == "s":
-            #     break;
+    try:
+        while True:
+            game = chess.pgn.read_game(pgn)
 
-        turn = board.turn
-        legal_moves = list(board.legal_moves)
+            if game is None:
+                break;
 
-        positions = {}
+            result = game.headers["Result"]
+            if result not in result_states:
+                continue;
 
-        for move in legal_moves:
-            value = 0
+            result = result_states[result]
 
-            board.push(move)
+            board = game.board()
+            for move in game.mainline_moves():
+                board.push(move)
+                serialized_board = serialize(board)
+                X.append(serialized_board)
+                y.append(result)
 
-            for square in chess.SQUARES:
-                color = board.color_at(square)
-                if color is None:
-                    continue
+            games_read += 1
+            print(f'games read: {games_read}')
+    except:
+        print('Encountered exception... continuing')
 
-                piece = board.piece_at(square).piece_type
-                if color != turn:
-                    value += get_piece_value(piece)
-                else:
-                    value += get_position_value(piece, color, square)
+    X = np.array(X).astype(np.float32)
+    y = np.array(y).astype(np.float32)
 
-            positions[move] = value
-            board.pop()
+    return X, y
 
-        best_move = min(positions, key=positions.get)
-        board.push(best_move)
-        print(board)
-        print('----------------')
 
-read_game()
+def read_games():
+    X = np.empty([1, 7, 8, 8])
+    y = np.empty([1, 7, 8, 8])
+
+    directory = 'data'
+    for filename in os.listdir(directory):
+        if filename.endswith(".pgn"):
+            tic = time.perf_counter()
+            X_pgn, y_pgn = read_game(os.path.join(directory, filename))
+            X = np.concatenate(( X, X_pgn ), axis=0) if X is not None else X_pgn
+            y = np.concatenate(( y, y_pgn ), axis=0) if y is not None else y_pgn
+            toc = time.perf_counter()
+            print(f"Read game {toc - tic:0.4f} seconds")
+        else:
+            continue
+
+    return X, y
+
+def train():
+    X, y = read_games()
+    X = np.moveaxis(X, 1, -1) # workaround for error, where tensorflow is expecting channels column last
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.15, random_state=42)
+
+    cnn = tf.keras.models.Sequential()
+
+    # Convolution
+    print('Convolution')
+    cnn.add(tf.keras.layers.Conv2D(filters=32, kernel_size=3, activation='relu'))
+
+    # Pooling
+    print('Pooling')
+    cnn.add(tf.keras.layers.MaxPool2D(pool_size=2, strides=2, padding='same'))
+
+    # Second convolution layer
+    # print('Second convolution layer')
+    # cnn.add(tf.keras.layers.Conv2D(filters=32, kernel_size=3, activation='relu'))
+    # cnn.add(tf.keras.layers.MaxPool2D(pool_size=2, strides=2))
+
+    # Flattening
+    print('Flattening')
+    cnn.add(tf.keras.layers.Flatten())
+
+    # Full connection
+    print('Full connection')
+    cnn.add(tf.keras.layers.Dense(units=128, activation='relu'))
+
+    # Output layer
+    print('Output layer')
+    cnn.add(tf.keras.layers.Dense(units=1, activation='sigmoid'))
+
+    # Compiling the CNN
+    print('Compiling')
+    cnn.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
+
+    # Training
+    print('Training')
+    cnn.fit(x=X_train, y=y_train, validation_split=0.2, epochs = 35)
+
+    cnn.save('model')
+    print("Saved model to disk")
+
+    example_board = X_test
+    result = y_test
+
+    prediction = cnn.predict(example_board)
+
+    print(prediction)
+    print(result)
+
+train()
---
+++ serialize.py
@@ -0,0 +1,45 @@
+import chess
+import numpy as np
+import math
+
+color_map = {
+    chess.WHITE: 1,
+    chess.BLACK: -1
+}
+
+piece_map = {
+    chess.PAWN: 0,
+    chess.KNIGHT: 1,
+    chess.BISHOP: 2,
+    chess.ROOK: 3,
+    chess.QUEEN: 4,
+    chess.KING: 5
+}
+
+def initialize():
+    # 8 x 8 x 7 dimensional array
+    # pawns
+    # knights
+    # bishops
+    # rooks
+    # queens
+    # kings
+    # whose turn is it next?
+    return np.zeros((7, 64), dtype=np.int8)
+
+def serialize(board):
+    state = initialize()
+
+    # squares starting in bottom right corner?
+    for square in chess.SQUARES:
+        color = board.color_at(square)
+
+        if color is None:
+            continue
+
+        piece = board.piece_at(square).piece_type
+        state[piece_map[piece], square] = 1 * color_map[color]
+
+    state[6] = board.turn * 1
+
+    return state.reshape(7, 8, 8)
